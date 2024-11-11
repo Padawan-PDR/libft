d39edef8af5e1e30fa4cac38d06bcc61a9899849
Padawan_PDR <pdr777dpr@gmail.com>
2024-11-11 00:29:05 -0300

    Terminado o split e começado o itoa

--- ft_split.c
+++ ft_split.c
@@ -6,51 +6,83 @@
 /*   By: pedrada <pedrada@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 18:49:37 by pedrada           #+#    #+#             */
-/*   Updated: 2024/11/10 03:31:26 by pedrada          ###   ########.fr       */
+/*   Updated: 2024/11/10 22:03:47 by pedrada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-int	strings_count(char const *s, char c)
+static int	ft_s_count(char const *s, char c)
 {
 	int	count;
+	int	i;
 
 	count = 0;
-	while (*s)
+	i = 0;
+	while (s[i])
 	{
-		if (*s != c && (*s + 1 == c || *(s + 1) == '\0'))
+		if ((s[i] != c && s[i + 1] == c) || (s[i + 1] == '\0'))
 			count++;
 		s++;
 	}
 	return (count);
 }
 
+static char	*ft_strncpy(char *dst, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dst[i] = '\0';
+		i++;
+	}
+	return (dst);
+}
+
+static char	*ft_strndup(const char *s, size_t n)
+{
+	char	*ptr;
+
+	ptr = (char *)malloc(1 + n * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	ptr = ft_strncpy(ptr, s, n);
+	ptr[n] = '\0';
+	return (ptr);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	int		i;
 	int		j;
-	char	**new_str;
-	int		aux;
+	int		k;
+	char	**new_strings;
 
-	aux = 0;
 	i = 0;
-	j = 0;
-	if (s[0] == '\0' || c == '\0')
+	k = 0;
+	new_strings = (char **)malloc((ft_s_count(s, c) + 1) * sizeof(char *));
+	if (!new_strings)
 		return (NULL);
-	new_str = (char **)malloc((strings_count(s, c) + 1) * sizeof(char *));
-	if (!new_str)
-		return (NULL);
-	while (s[i] != '\0')
+	while (s[i])
 	{
-		if (s[i] == c)
+		while (s[i] == c)
+			i++;
+		j = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > j)
 		{
-			if (j != 0)
-				aux = i;
-			ft_strlcpy(new_str[j], s + aux, i);
-			j++;
+			new_strings[k] = ft_strndup(s + j, i - j);
+			k++;
 		}
-		i++;
 	}
-	return (new_str);
+	new_strings[k] = NULL;
+	return (new_strings);
 }
