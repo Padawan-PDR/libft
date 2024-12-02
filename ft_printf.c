31457e594b923c438a54f7afaa0b36d5c6fdb39d
Pedro Almeida Freires <pedroalm@c1r3p4.42sp.org.br>
2024-12-02 10:49:22 -0300

    estamos evoluindo na construção da função

--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pedrada <pedrada@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pedroalm <pedroalm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 01:00:39 by pedrada           #+#    #+#             */
-/*   Updated: 2024/11/28 14:28:30 by pedrada          ###   ########.fr       */
+/*   Updated: 2024/12/02 10:48:45 by pedroalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,40 @@
 static int	n_conversions(const char *s)
 {
 	int	i;
-	int	n_conversions;                               
+	int	n_conversions;
+	int	aux;
 
 	i = 0;
 	n_conversions = 0;
+	aux = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] == '%')
 		{
 			n_conversions++;
+			aux = 1;
 		}
+		if (aux == 0)
+			write(1, s[i], 1);
 		i++;
 	}
-	return(n_conversions);
+	return (n_conversions);
 }
-
 
 int	ft_printf(const char *s, ...)
 {
-	if(s == NULL || *s == '\0'_
+	if (s == NULL || *s == '\0')
 		return (NULL);
-	n_conversios(s);
-	
+	n_conversions(s);
+	ft_strchr(s, '%');
+	if (*s + 1 != 'c' || \
+	*s + 1 != 's' || \
+	*s + 1 != 'p' || \
+	*s + 1 != 'd' || \
+	*s + 1 != 'i' || \
+	*s + 1 != 'u' || \
+	*s + 1 != 'x' || \
+	*s + 1 != 'X' || \
+	*s + 1 != '%')
+		return (NULL);
 }
-
---
+++ ft_printf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pedroalm <pedroalm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 09:56:48 by pedroalm          #+#    #+#             */
+/*   Updated: 2024/12/02 10:46:07 by pedroalm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include </nfs/homes/pedroalm/libft_do_github/libft/libft.h>
+
+#endif
