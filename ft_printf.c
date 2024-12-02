05b262451597014cd2cdc12386401472793d484f
Pedro Almeida Freires <pedroalm@c1r4p4.42sp.org.br>
2024-12-02 17:20:22 -0300

    em andamento

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,14 @@
 /*   By: pedroalm <pedroalm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 01:00:39 by pedrada           #+#    #+#             */
-/*   Updated: 2024/12/02 10:48:45 by pedroalm         ###   ########.fr       */
+/*   Updated: 2024/12/02 17:16:02 by pedroalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	n_conversions(const char *s)
+
+static int	count_conversions(const char *s)
 {
 	int	i;
 	int	n_conversions;
@@ -28,20 +29,20 @@ static int	n_conversions(const char *s)
 			n_conversions++;
 			aux = 1;
 		}
-		if (aux == 0)
+		if (!aux)
 			write(1, s[i], 1);
 		i++;
 	}
 	return (n_conversions);
 }
-
-int	ft_printf(const char *s, ...)
+static void	conversion(const char *sleep)
 {
-	if (s == NULL || *s == '\0')
-		return (NULL);
-	n_conversions(s);
-	ft_strchr(s, '%');
-	if (*s + 1 != 'c' || \
+	
+	if (*s + 1 == 'c')
+	{
+	va_start(args, char);
+		write(1, );
+	}
 	*s + 1 != 's' || \
 	*s + 1 != 'p' || \
 	*s + 1 != 'd' || \
@@ -49,6 +50,18 @@ int	ft_printf(const char *s, ...)
 	*s + 1 != 'u' || \
 	*s + 1 != 'x' || \
 	*s + 1 != 'X' || \
-	*s + 1 != '%')
+	*s + 1 != '%');
+		
+}
+
+int	ft_printf(const char *s, ...)
+{
+	int	printed;
+	
+	if (s == NULL || *s == '\0')
 		return (NULL);
+	printed = count_conversions(s);
+	va_list	args;
+	va_start(args, printed);
+	
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: pedroalm <pedroalm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 09:56:48 by pedroalm          #+#    #+#             */
-/*   Updated: 2024/12/02 10:46:07 by pedroalm         ###   ########.fr       */
+/*   Updated: 2024/12/02 17:10:08 by pedroalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FT_PRINTF_H
 
 # include <stdlib.h>
+# include <stdarg.h>
 # include </nfs/homes/pedroalm/libft_do_github/libft/libft.h>
 
 #endif
